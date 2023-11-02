eb64fefa722481d863cae7a478fac4244bb7c34f
HeAndres <anhees@protonmail.com>
2023-11-02 08:23:25 +0100

    add try except block for cron to work in any case

--- script.py
+++ script.py
@@ -1,14 +1,25 @@
-import logging
+try:
 
-# Configure the logging settings
-logging.basicConfig(
-    filename='log.log',
-    level=logging.INFO,  # Set the minimum logging level
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-)
+    import logging
 
-# Create a logger instance
-logger = logging.getLogger("Logger")
+    # Configure the logging settings
+    logging.basicConfig(
+        filename='log.log', #/app/logs/
+        level=logging.INFO,  # Set the minimum logging level
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
 
-# Log a message
-logger.info("Log message")
+    # Create a logger instance
+    logger = logging.getLogger("Logger")
+
+    # Log a message
+    logger.info("Log message")
+
+    print('chron executed')
+
+    with open('a', 'a') as f:
+        f.write('Hola')
+        
+except:
+    
+    print('Error in script')
